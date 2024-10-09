c5f3f579e614c56540575299ecbc4ce1b955cde9
paolabronqueti <125603242+paolabronqueti@users.noreply.github.com>
2024-10-09 14:53:31 -0300

    09/10

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,20 @@
+const url = 'https ://raw.githubusercontent.com/paola/api/main/dados-globai'
+
+async function vizualizarInformacoesGlobais() {}
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadads/1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo/ 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((pessoasConectadas/ pessoasNoMundo) * 100)
+    const porcentagemConectada = ((pessoasConectadas/ pessoasNoMundo)*100).toFixed
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML= ` Vocễ sabia que o mundo tem <span>${pessoasNoMundo}bilhões
+
+    const container= document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+                                                }
+
+vizualizarInformacoesGlobais() 
